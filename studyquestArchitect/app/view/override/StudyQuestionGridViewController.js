4ee12aabf75a114601becc8aebd3df70fdf1e83b
sabashadi <saba.chadi.michel@gmail.com>
2016-04-04 22:55:26 +0200

    index change

---
+++ studyquestArchitect/app/view/override/StudyQuestionGridViewController.js
@@ -0,0 +1,136 @@
+Ext.define('MyApp.view.override.StudyQuestionGridViewController', {
+    override: 'MyApp.view.StudyQuestionGridViewController',
+
+    onStudyQuestionGridIdChHist: function() {
+
+    },
+
+    onStudyQuestionGridIdAfterRender: function(component, eOpts) {
+        component.getPlugin('gridediting').lockGrid(false);
+
+    var params;
+            
+   
+        this.getResultArray(
+        	    function(data){
+        	            Utility.grid.loadGrid(component,data,component.getViewModel().getStore('StudyQuestionStore'));
+        	        });
+    },
+
+    onStudyQuestionGridIdInEdit: function() {
+
+    },
+
+    onStudyQuestionGridIdResetEdit: function(gridpanel,promptWin) {
+        Utility.grid.resetEdit(this.getView(),this.getResultArray(),this.getView().getViewModel().getStore('StudyQuestionStore'),promptWin);
+
+    },
+
+   onStudyQuestionGridIdSaveEdit: function(gridpanel, promptWin, dataToBeSaved, comment) {
+
+	     var me=this;
+	     var params={};
+	            params.table="study_quest";
+	            params.idName="studyQuestId";
+	            params.dataToBeSaved=dataToBeSaved;
+	            params.comment=comment;
+	            var result=[];
+	            Server.CommonQueries.saveRecords(params,
+	                function(_result){
+	                    if(_result.success){
+	                        var resultArray=[];
+	                        this.getResultArray(function(data){
+	                            Utility.grid.saveEdit(me.getView(),data,me.getView().getViewModel().getStore('StudyQuestionStore'),promptWin);
+	                        },this);
+	                    }
+	                    else{
+	                        console.error(_result.msg);
+	                        Ext.MessageBox.alert("Error","save Error "+_result.msg);
+	                    }
+	                },me
+	            );
+     
+        
+    },
+
+    onStudyQuestionGridIdAddItem: function() {
+
+        var rec = new MyApp.model.StudyQuestionModel({
+            added: true,
+            modified: false,
+            addedAndValidated:false,
+            toDelete: false
+        });
+        Utility.grid.addItem(this.getView(),rec);
+    },
+
+    onStudyQuestionGridIdDeleteItem: function() {
+        Utility.grid.deleteItem(this.getView());
+    },
+
+    onStudyQuestionGridIdModifyItem: function() {
+        Utility.grid.modifyItem(this.getView());
+    },
+
+    onStudyQuestionGridIdQuitEdit: function(gridpanel,promptWin) {
+        Utility.grid.quitEdit(this.getView(),this.getResultArray(),this.getView().getViewModel().getStore('StudyQuestionStore'),promptWin);
+    },
+    onStudyQuestionGridIdBeforeEdit: function(editor,context) {
+        return (Utility.grid.beforeEdit(editor,context));
+    },
+
+ 
+
+    onStudyQuestionGridIdCanceledit: function(editor,context) {
+        return(Utility.grid.cancelEdit(editor,context));
+    },
+
+    onStudyQuestionGridIdContainerClick: function(dataview, e, eOpts) {
+        return(Utility.grid.gridContainerClick(this.getView()));
+    },
+
+    onStudyQuestionGridIdEdit: function(editor,context) {
+ // var columnsName=['name','text'];
+    	
+    	var columnsName=['studyQuestText','active'];
+        Utility.grid.edit(editor, context, columnsName);
+    },
+
+    onStudyQuestionGridIdBeforeCellClick: function(tableview, td, cellIndex, record, tr, rowIndex, e, eOpts) {
+        Utility.grid.viewBeforeCellClick(this.getView());
+    },
+    
+
+    onStudyQuestionGridIdValidateedit: function(editor, context) {
+        
+        var check;
+        check=true;
+        // check= checkFunction(); check if all required data in the form editor
+        
+        return(Utility.grid.validateedit(editor,context,check));
+    },
+    getResultArray:function(callback)
+    {
+        var me=this;
+        var params={
+        		table:"study_quest"
+        };
+        var result=[];
+        Server.CommonQueries.read(params,
+                function(res){
+                    if(res.success){
+                    	callback(res.data);
+                    }
+                    else{
+                        console.error(res.msg);
+                        callback(res.msg);
+                    }
+                });
+    },
+    /*********************** renderers****************************************************/
+  /**xxComboboxRenderer**/
+    
+ 
+    
+
+});
