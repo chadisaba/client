dbbcc3e198c8f11409cc86480c366b2f5e311d82
fadisaba <saba.fadi@wanadoo.fr>
2016-05-30 17:47:56 +0200

    Update StudyDirect.js

--- Common/direct/StudyDirect.js
+++ Common/direct/StudyDirect.js
@@ -28,5 +28,90 @@ var StudyDirect={
 
                     });
         }
+    },
+     getStudyByCodeFromIndexedDB:function(_searchValue,_doctorId)
+    {
+        //Creating a promise
+        var promise=new Promise(
+            function(resolve, reject) {
+                smartmedDB.study.where("studyCode")
+                .startsWithIgnoreCase(_searchValue)
+                .and(function(_study) { return _study.doctorId == _doctorId; })
+                .toArray (function (_resultsArray) {
+                resolve(_resultsArray);
+                });
+                .toArray (function (_resultsArray) {
+              resolve(_resultsArray);
+          });
+            
+             });
+         return promise;
+    },
+     getStudyByNameFromIndexedDB:function(_searchValue,_doctorId)
+    {
+        //Creating a promise
+        var promise=new Promise(
+            function(resolve, reject) {
+                smartmedDB.study.where("studyName")
+                .startsWithIgnoreCase(_searchValue)
+                .and(function(_study) { return _study.doctorId == _doctorId; })
+                .toArray (function (_resultsArray) {
+              resolve(_resultsArray);
+          });
+            
+             });
+         return promise;
+    }
+     studyAutoComplete:function(_scope,_serachValue,_studyComboStore,_field,_fromIndexedDB,_searchLengh)
+    {
+        var me=_scope;
+        var searchLengh=_searchLengh||4;
+        if(_serachValue && isNaN(_serachValue) && !stringUtil.isUUID4(_serachValue))
+        {
+        if(_serachValue.length>=searchLengh)
+        {
+            var store = me.getViewModel().getStore(_studyComboStore);
+            if(_fromIndexedDB)
+            {
+               this.getCitiesFromIndexedDB(_serachValue)
+                .then(
+                    function(cityData)
+                    {
+                        store.clearFilter();
+                        store.removeAll();
+                        store.loadData(cityData);
+
+                        store.filter({
+                            property: 'cityName',
+                            anyMatch: true,
+                            value   : _serachValue
+                        });
+                        _field.expand();
+
+
+                    });   
+            }
+            else{
+              this.getCities(_serachValue.toUpperCase())
+                .then(
+                    function(cityData)
+                    {
+                        store.clearFilter();
+                        store.removeAll();
+                        store.loadData(cityData);
+
+                        store.filter({
+                            property: 'cityName',
+                            anyMatch: true,
+                            value   : _serachValue
+                        });
+                        _field.expand();
+
+
+                    });  
+            }
+            
+        }
+    }
     }
 };
